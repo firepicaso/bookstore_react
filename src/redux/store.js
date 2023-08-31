3c830b0e78b3fa93b5aabd929420470179785821
Mustakim <m.m.kiron@gmail.com>
2023-08-31 17:12:23 +0600

    Configure reducers

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,11 @@
-import { combineReducers, conbineReducers, configureStore } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import booksReducer from './books/bookSlice';
 import categoriesReducer from './catagories/categoriesSlice';
 
-const rootRuducer = combineReducers({
-  books: booksReducer,
-  categories: categoriesReducer,
-});
-
 const store = configureStore({
   reducer: {
-    rootRuducer,
+    books: booksReducer,
+    categories: categoriesReducer,
   },
 });
 
