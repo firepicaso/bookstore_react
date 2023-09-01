e466935bac5188221eb8dfc6124c40e15968371f
Mustakim <m.m.kiron@gmail.com>
2023-09-01 21:20:57 +0600

    Use simpler configureStore function

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,12 @@
-import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import booksReducer from './books/bookSlice';
 import categoriesReducer from './catagories/categoriesSlice';
 
-const rootReducer = combineReducers({
-  books: booksReducer,
-  categories: categoriesReducer,
+const store = configureStore({
+  reducer: {
+    books: booksReducer,
+    categories: categoriesReducer,
+  },
 });
-
-const store = configureStore({ reducer: rootReducer });
 
 export default store;
